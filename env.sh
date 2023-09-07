35f02cbf59d3230448b505f2ea7e06bd05d61208
Satish <satish.bomma@gmail.com>
2023-09-07 11:11:55 -0500

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,10 @@
+python -m pip install --upgrade pip
+pip install pip virtualenv
+pip install streamlit
+pip install elasticsearch
+pip install PyYAML
+pip install numpy scipy pandas
+pip install pyspark
+pip install accelerate
+pip install git+https://github.com/huggingface/transformers
+echo "SUCCESS!"
